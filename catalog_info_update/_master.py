97d8b1a455469c4670df1f13676a0c8452a00b98
Noé MARIUS <noe.marius@gmail.com>
2021-12-20 16:49:49 +0100

    wrote supplier image, run, change image script, no test

---
+++ catalog_info_update/_master.py
@@ -0,0 +1,13 @@
+#TODO:
+# supplier data
+# text file like json to api
+# supplier img
+# resize
+# format change 4chan to 3chan
+# Uploading images to web server
+# post images to site
+# post desc to site
+# Generate a PDF report and send it through email
+# Script to generate a PDF report
+# Send report through email
+# Health check
