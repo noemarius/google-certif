a204a93b6d9be7cdac26564f79c46921e04b305d
noemarius <noe.marius@gmail.com>
2021-12-20 23:32:09 +0100

    fixed health check

--- week4/health_check.py
+++ week4/health_check.py
@@ -37,11 +37,11 @@ def hostname_check():
 
 def email_warning(error):
   sender = "automation@example.com"
-  receiver = "{}@example.com".format(os.environ["USER"])
+  recipient = "{}@example.com".format(os.environ["USER"])
   subject = error
   body = "Please check your system and resolve the issue as soon as possible."
-  message = emails.generate_email(sender, receiver, subject, body)
-  emails.send_email(sender, message)
+  message = emails.generate_email(sender, recipient, subject, body).set_params()
+  emails.send_email(message)
 
 if not cpu_check():
   subject = "Error - CPU usage is over 80%"
